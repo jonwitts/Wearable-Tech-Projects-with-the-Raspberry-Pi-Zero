2b8aebf32e2a43995bce740f4363420b0515ada0
Jon Witts <jonwitts@users.noreply.github.com>
2020-07-08 11:54:58 +0100

    fix typo in comment

--- Chapter08/pulseRead.py
+++ Chapter08/pulseRead.py
@@ -123,7 +123,7 @@ while True:
             runningTotal += rate[9]  # add the latest IBI to runningTotal
             runningTotal /= 10       # average the last 10 IBI values 
             BPM = 60000/runningTotal # how many beats can fit into a minute? that's BPM!
-            printBPM(0.25, str(int(BPM))) # we have a BLM lets display it
+            printBPM(0.25, str(int(BPM))) # we have a BPM lets display it
 
         if Signal < thresh and Pulse == True : # when the values are going down, the beat is over
             Pulse = False # reset the Pulse flag so we can do it again
